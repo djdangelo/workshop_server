cf833a21fdf5d534510c4845c89b46461991f275
Dangelo Aguilar <48798490+djdangelo@users.noreply.github.com>
2022-10-14 23:17:58 -0600

    notifications ready

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const db = require('./database/config');
 const cors = require('cors');
 
 const app = express();
-
-app.set('port', process.env.PORT);
 app.use(cors());
+app.set('port', process.env.PORT);
+
 app.use(express.json());
 app.use(morgan('dev'));
 
@@ -19,6 +19,14 @@ app.use(`${ process.env.GLOBAL_ROUTE }users`, require('./routes/user.route'));
 app.use('/*', async (req, res) => {
     return messagesShared.messageNotAuthUser(res, 'You are not allowed this action.', []);
 });
+
+module.exports.io = require('socket.io')(server,{
+    cors: {
+        origin: "http://localhost:4200",
+            methods: ["GET", "POST"]
+    }
+});
+require('./sockets/socket');
 
 server.listen(app.get('port'),() => {
     console.log('Server on port',app.get('port'));
--- sockets/socket.js
+++ sockets/socket.js
@@ -3,10 +3,21 @@ const messageShared = require('../shared/messages.shared');
 
 io.on('connection', client => {
 
-    const  id  = client.handshake.headers['x-token'];
+    const  id  = client.handshake.headers['my-id'];
+    if (id === undefined) { return client.disconnect(); }
+
     console.log('Client is connected.');
-    if (id !== null) { return client.disconnect(); }
+
     client.join(id);
+
+    client.on('notifications', (payload) => {
+        io.emit('list-notifications', payload);
+    });
+
+    client.on('private-notifications', (payload) => {
+       io.to(payload.id).emit('private-notifications', payload);
+    });
+
 
     client.on('disconnect', () => {
         console.log('Client is disconnected.');
